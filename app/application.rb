9f02cbeb9a9fb951464ba4b43f913249ca7b0f65
mandareis <amanda@depaula.xyz>
2020-10-26 10:21:14 -0700

    commit tree c825873a2ed30816ce57969fb679602929f0a763

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if @@items.include?(item)
+      resp.write "#{item.price}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
